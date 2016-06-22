7275db0a366075c31fbf82d4a98a80e8a8e08cb0
Michael Walter <michael.walter@gmail.com>
2016-06-22 12:31:43 -0700

    Initial commit (setup and testing infrastructure).

---
+++ README.md
@@ -0,0 +1,3 @@
+# Barvikron
+
+Efficiently compute Kronecker coefficients of bounded height using the technique of [arXiv:1204.4379](http://arxiv.org/abs/1204.4379).
---
+++ setup.py
@@ -0,0 +1,58 @@
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+import ast, io, re, os.path, sys
+
+# determine __version__ from pw.py source (adapted from mitsuhiko)
+VERSION_RE = re.compile(r'__version__\s+=\s+(.*)')
+with io.open('barvikron/__init__.py', encoding='utf-8') as fp:
+    version_code = VERSION_RE.search(fp.read()).group(1)
+    version = str(ast.literal_eval(version_code))
+
+# read long description and convert to RST
+long_description = io.open(
+    os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), 'README.md'),
+    encoding='utf-8').read()
+try:
+    import pypandoc
+    long_description = pypandoc.convert(long_description, 'rst', format='md')
+except ImportError:
+    pass
+
+
+# run pytest (from https://pytest.org/latest/goodpractices.html)
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+
+setup(
+    name='barvikron',
+    version=version,
+    description='Efficiently compute Kronecker coefficients of bounded height (using the technique of arXiv:1204.4379).',
+    long_description=long_description,
+    author='Michael Walter',
+    author_email='michael.walter@gmail.com',
+    url='https://github.com/catch22/barvikron',
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+    ],
+    install_requires=['Click'],
+    packages=['barvikron'],
+    tests_require=['pytest'],
+    cmdclass={'test': PyTest})
